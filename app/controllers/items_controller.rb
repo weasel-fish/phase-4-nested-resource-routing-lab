ae1c2746de6d456343e54230d2558ad9b54a7d0c
github username <github email address>
2021-07-21 11:17:14 -0400

    commit tree d339d409bed5c2db39247c3344125439a68c3bed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,30 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create(name: params[:name], description: params[:description], price: params[:price], user_id: params[:user_id])
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: {error: "Not Found"}, status: :not_found
   end
 
 end
